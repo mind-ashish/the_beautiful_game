6dbcacbf3a18304fb2cdf8fe047e1689cd085474
Aashish Choudhary <aashish01official@gmail.com>
2020-10-19 16:03:47 +0530

    controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,6 @@ module.exports.home=function(req,res){
     return res.render('home',{   //path is wrt views
         layout:'home_layout',  //setting custom layout for home page, than default layout
         title:'The-beautiful-game'
-
+        
     });
 }
