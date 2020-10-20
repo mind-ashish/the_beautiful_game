59038b3a681118a72efc1f3d30a766b31188fc81
Aashish Choudhary <aashish01official@gmail.com>
2020-10-20 16:11:57 +0530

    generating paths

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -6,24 +6,19 @@ const environment=require('./environment');
 const fs=require('fs');
 const path=require('path');
 module.exports.pathSetter=function(app){      //to modify app.locals , passing app as an arguement to exported fn.
-    console.log(environment.name);
-    try{
-        
-        console.log(path.join(__dirname,'../','public','abc.txt'));
-
-        const data=fs.readFileSync('../public/abc.txt','utf8');
-        console.log("Data"+ data);
-    }catch(err){
-        console.log(err);
-    }
-    
-    
     app.locals.path=function(filePath){   //making path fn in app.locals
         
         if(environment.name=='development'){
-            return filePath;
+            return filePath; //assetpath is given in app.js , so filepath is wrt to it
         }else{
-            return '/';
+            const data=fs.readFileSync(path.join(__dirname,'../','public','rev-manifest.json'),'utf8');
+            //reading rev-manifest.json file from public folder and giving path from json key-value
+            var obj=JSON.parse(data);
+            // console.log(filePath);
+            if(filePath.indexOf('./')>-1){
+                filePath=filePath.substring(2);
+            }
+            return './'+obj[filePath];
         }
     }
 }
