1b24f59df2430e633798090462a0ba5b221d5f0b
Aashish Choudhary <aashish01official@gmail.com>
2020-10-20 15:12:34 +0530

    try

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -3,8 +3,21 @@
 // }
 
 const environment=require('./environment');
+const fs=require('fs');
+const path=require('path');
 module.exports.pathSetter=function(app){      //to modify app.locals , passing app as an arguement to exported fn.
     console.log(environment.name);
+    try{
+        
+        console.log(path.join(__dirname,'../','public','abc.txt'));
+
+        const data=fs.readFileSync('../public/abc.txt','utf8');
+        console.log("Data"+ data);
+    }catch(err){
+        console.log(err);
+    }
+    
+    
     app.locals.path=function(filePath){   //making path fn in app.locals
         
         if(environment.name=='development'){
