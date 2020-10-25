7519e761c380db20a97bef11dae7af0dc1c1086a
Aashish Choudhary <aashish01official@gmail.com>
2020-10-25 12:11:20 +0530

    restructure layouts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,7 @@
 module.exports.home=function(req,res){
    // throw new Error('hello');
     return res.render('home',{   //path is wrt views
-        layout:'home_layout',  //setting custom layout for home page, than default layout
+        layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout
         title:'The-beautiful-game'
         
     });
