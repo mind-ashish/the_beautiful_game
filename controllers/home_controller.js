5e7e69c088a44cc4f30ce890cb10b4ebf2f7a19c
Aashish Choudhary <aashish01official@gmail.com>
2020-10-25 12:48:50 +0530

    login and signup

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 
 module.exports.home=function(req,res){
    // throw new Error('hello');
-    return res.render('home',{   //path is wrt views
+    return res.render('login',{   //path is wrt views
         layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout
         title:'The-beautiful-game'
         
