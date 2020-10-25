14c33d95f4673ca529720b661c8617deee451476
Aashish Choudhary <aashish01official@gmail.com>
2020-10-25 14:34:11 +0530

    profile pic and about

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 
 module.exports.home=function(req,res){
    // throw new Error('hello');
-    return res.render('changePassword',{   //path is wrt views
+    return res.render('completeDetails',{   //path is wrt views
         layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout
         title:'The-beautiful-game'
         
