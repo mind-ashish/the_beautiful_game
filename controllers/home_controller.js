b3c98a347344adcb3c31dd3e5eb77ae364f39566
Aashish Choudhary <aashish01official@gmail.com>
2020-10-25 14:53:20 +0530

    basic html design

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,14 @@
-
+var leagues=['Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1'];
+var teams=['dummy1','dumm2'];
+var players=['a','b','c'];
+//to change & complete
 module.exports.home=function(req,res){
    // throw new Error('hello');
-    return res.render('completeDetails',{   //path is wrt views
+    return res.render('players',{   //path is wrt views
         layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout
-        title:'The-beautiful-game'
-        
+        title:'The-beautiful-game',
+        leagues:leagues,
+        teams:teams,
+        players:players
     });
 }
