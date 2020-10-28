4025f9602f29ce15158ae96b314289f60c98e6ca
Aashish Choudhary <aashish01official@gmail.com>
2020-10-28 13:17:58 +0530

    corrected responsive issue

--- assets/js/user_layout_script.js
+++ assets/js/user_layout_script.js
@@ -4,21 +4,14 @@
     function navigationToggler(){
       var flag=window.matchMedia('(max-width:768px)').matches;
       if(flag && ($('#navigation').parent('#main-content').length==0)){
-        alert('done1');
           var element=$('#navigation').remove();
-          console.log(element);
           $('#main-content').append(element);
-          alert('done2');
       }
-      // flag=window.matchMedia('(min-width:768px)').matches;
-      // if(flag && ($('#navigation').parent('#navigation_container').length==0)){
-      //   alert('hello1');
-      //   var element=$('#navigation').detach();
-        
-      //   $('#navigation-container').append(element);
-      //   alert('hello2');
-        
-      // }
+      flag=window.matchMedia('(min-width:768px)').matches;
+      if(flag && ($('#navigation').parent('#navigation_container').length==0)){
+        var element=$('#navigation').remove();    
+        $('#navigation_container').append(element);        
+      }
     }
     window.addEventListener('load',navigationToggler);
     window.addEventListener('resize',navigationToggler);
