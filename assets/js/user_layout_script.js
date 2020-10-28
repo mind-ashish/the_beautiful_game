de2c63c35b06234b6d729ec72ded93c899d534c9
Aashish Choudhary <aashish01official@gmail.com>
2020-10-28 12:45:39 +0530

    toggler

--- assets/js/user_layout_script.js
+++ assets/js/user_layout_script.js
@@ -1,7 +1,27 @@
-$(document).ready(function () {
-
     $('.my-button').on('click', function () {
-  
       $('.animated-icon').toggleClass('open');
     });
-  });
+    function navigationToggler(){
+      var flag=window.matchMedia('(max-width:768px)').matches;
+      if(flag && ($('#navigation').parent('#main-content').length==0)){
+        alert('done1');
+          var element=$('#navigation').remove();
+          console.log(element);
+          $('#main-content').append(element);
+          alert('done2');
+      }
+      // flag=window.matchMedia('(min-width:768px)').matches;
+      // if(flag && ($('#navigation').parent('#navigation_container').length==0)){
+      //   alert('hello1');
+      //   var element=$('#navigation').detach();
+        
+      //   $('#navigation-container').append(element);
+      //   alert('hello2');
+        
+      // }
+    }
+    window.addEventListener('load',navigationToggler);
+    window.addEventListener('resize',navigationToggler);
+    
+
+
