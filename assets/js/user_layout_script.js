b7f3b1d393c8e1664eca0ddf4620050d8d35efcb
Aashish Choudhary <aashish01official@gmail.com>
2020-10-28 17:00:37 +0530

    fixed 3 issues

--- assets/js/user_layout_script.js
+++ assets/js/user_layout_script.js
@@ -1,26 +1,39 @@
-    $('.my-button').on('click', function () {
-      $('.animated-icon').toggleClass('open');
-    });
-    function navigationToggler(){
-      var flag=window.matchMedia('(max-width:768px)').matches;
-      if(flag && ($('#navigation').parent('#main-content').length==0)){
-          var element=$('#navigation').remove();
-          $('#main-content').append(element);
-      }
-      flag=window.matchMedia('(min-width:768px)').matches;
-      if(flag && ($('#navigation').parent('#navigation_container').length==0)){
-        var element=$('#navigation').remove();    
-        $('#navigation_container').append(element);        
-      }
+(function(){
+  $('.my-button').on('click', function () {
+    $('.animated-icon').toggleClass('open');
+    
+  });
+  
+  function navigationToggler(){
+    
+    var flag=window.matchMedia('(max-width:768px)').matches;
+    if(flag && ($('#navigation').parent('#main-content').length==0)){
+        // var classList = $('.animated-icon').attr('class').split(/\s+/);
+        // console.log(`classList: ${classList}`);
+
+        var element=$('#navigation').remove();
+        $('#main-content').append(element);
+        $('#navigation').addClass('collapse');
     }
-    window.addEventListener('load',navigationToggler);
-    window.addEventListener('resize',navigationToggler);
+
+    flag=window.matchMedia('(min-width:768px)').matches;
+    if(flag){
+      $('#navigation').removeClass('collapse');
+    }
+    if(flag && ($('#navigation').parent('#navigation_container').length==0)){
+
+      var element=$('#navigation').remove();    
+      $('#navigation_container').append(element);     
+      //$('#navigation').removeClass('collapse');
+    }
+  }
+  window.addEventListener('load',navigationToggler);
+  window.addEventListener('resize',navigationToggler);
+})();
+   
 
 
-    // to do
-    // large resolution --> small resultion : toggle closed
-    // small resultion --> large resolution: toggle open
-    // small resolution onload : topggle closed
+    
     
 
 
