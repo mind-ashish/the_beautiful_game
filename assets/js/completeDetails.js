5d775e4eee0088769e9d73af7b7c1fdbc271d527
Aashish Choudhary <aashish01official@gmail.com>
2020-11-03 22:57:01 +0530

    fixes same re- image upload issue

--- assets/js/completeDetails.js
+++ assets/js/completeDetails.js
@@ -12,9 +12,11 @@
       }
     $("#image-holder").append(img);
 
-    $("#image-holder img").on('click',function(){
+    $("#image-holder").on('click',function(){
         $("#image-holder img").remove();
+        document.getElementById('avatar').value=""; //this empties the fileList from input type =file. So removing file and uploading same works fine now.
     });
-    //todo:reupload of same image doesn't work: fix tommorow.
 }
 })();
+
+//note document.getElementById('avatar').files works in js but $("#avatar").files doesn't in jquery since part after . in most case is undefined in jquery docs, so check before u use.
