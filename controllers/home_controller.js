47762f657317382997cd9eeb8115b9fe44f1af3e
Aashish Choudhary <aashish01official@gmail.com>
2020-11-03 15:37:07 +0530

    made pick players page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,14 @@
-var leagues=['Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1'];
+var leagues=['Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1','Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1','Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1'];
 var teams=['dummy1','dumm2'];
 var players=['a','b','c'];
 //to change & complete
 module.exports.home=function(req,res){
    // throw new Error('hello');
-    return res.render('./home/changePassword.ejs',{   //path is wrt views
+    return res.render('./home/players.ejs',{   //path is wrt views
         layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout which is typically layout.ejs
         title:'The-beautiful-game',
         leagues:leagues,
         teams:teams,
-        players:players
+        players:leagues
     });
 }
