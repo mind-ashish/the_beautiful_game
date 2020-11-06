9c5fc8d46ee7e2920e33a1797fcbbfabe532c828
Aashish Choudhary <aashish01official@gmail.com>
2020-11-06 11:53:44 +0530

    home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,53 +1,55 @@
-var leagues=['Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1'];
-var teams=['dummy1','dumm2'];
-var players=['a','b','c'];
-//to change & complete
+// var leagues=['Premier league', 'La liga', 'Bundesliga', 'Serie A','Uber eats Ligue 1'];
+// var teams=['dummy1','dumm2'];
+// var players=['a','b','c'];
+
+var title='The-beautiful-game';
+var layout='./layouts/home_layout';
 module.exports.home=function(req,res){
     return res.render('./home/home.ejs',{   //path is wrt views
-        layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout which is typically layout.ejs
-        title:'The-beautiful-game',
+        layout:layout, //setting custom layout for home page, than default layout which is typically layout.ejs
+        title:title
     });
 }
 
 module.exports.login=function(req,res){
     return res.render('./home/login.ejs',{   
-        layout:'./layouts/home_layout', 
-        title:'The-beautiful-game',
+        layout:layout,
+        title:title
     });
 
 }
 module.exports.signup=function(req,res){
     return res.render('./home/signup.ejs',{   
-        layout:'./layouts/home_layout', 
-        title:'The-beautiful-game',
+        layout:layout,
+        title:title
     });
 
 }
 module.exports.goodbye=function(req,res){
     return res.render('./home/goodbye.ejs',{   
-        layout:'./layouts/home_layout', 
-        title:'The-beautiful-game',
+        layout:layout,
+        title:title
     });
 
 }
 module.exports.forgotPassword=function(req,res){
     return res.render('./home/forgotPassword.ejs',{   
-        layout:'./layouts/home_layout', 
-        title:'The-beautiful-game',
+        layout:layout,
+        title:title
     });
 
 }
 module.exports.changePassword=function(req,res){
     return res.render('./home/changePassword.ejs',{   
-        layout:'./layouts/home_layout', 
-        title:'The-beautiful-game',
+        layout:layout,
+        title:title
     });
 
 }
 module.exports.admin=function(req,res){
     return res.render('./home/adminInput.ejs',{   
-        layout:'./layouts/home_layout', 
-        title:'The-beautiful-game',
+        layout:layout,
+        title:title
     });
 
 }
