1a0fad4a938bd7107dd2e24770636457eb0fa8e5
Aashish Choudhary <aashish01official@gmail.com>
2020-11-06 11:34:01 +0530

    home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,12 +3,51 @@ var teams=['dummy1','dumm2'];
 var players=['a','b','c'];
 //to change & complete
 module.exports.home=function(req,res){
-   // throw new Error('hello');
-    return res.render('./home/completeDetails.ejs',{   //path is wrt views
+    return res.render('./home/home.ejs',{   //path is wrt views
         layout:'./layouts/home_layout',  //setting custom layout for home page, than default layout which is typically layout.ejs
         title:'The-beautiful-game',
-        leagues:leagues,
-        teams:teams,
-        players:players
     });
+}
+
+module.exports.login=function(req,res){
+    return res.render('./home/login.ejs',{   
+        layout:'./layouts/home_layout', 
+        title:'The-beautiful-game',
+    });
+
+}
+module.exports.signup=function(req,res){
+    return res.render('./home/signup.ejs',{   
+        layout:'./layouts/home_layout', 
+        title:'The-beautiful-game',
+    });
+
+}
+module.exports.goodbye=function(req,res){
+    return res.render('./home/goodbye.ejs',{   
+        layout:'./layouts/home_layout', 
+        title:'The-beautiful-game',
+    });
+
+}
+module.exports.forgotPassword=function(req,res){
+    return res.render('./home/forgotPassword.ejs',{   
+        layout:'./layouts/home_layout', 
+        title:'The-beautiful-game',
+    });
+
+}
+module.exports.changePassword=function(req,res){
+    return res.render('./home/changePassword.ejs',{   
+        layout:'./layouts/home_layout', 
+        title:'The-beautiful-game',
+    });
+
+}
+module.exports.admin=function(req,res){
+    return res.render('./home/adminInput.ejs',{   
+        layout:'./layouts/home_layout', 
+        title:'The-beautiful-game',
+    });
+
 }
--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,14 @@ router.get('/signup',homeController.signup);
 router.get('/goodbye',homeController.goodbye);
 router.get('/forgotPassword',homeController.forgotPassword);
 router.get('/changePassword',homeController.changePassword);
+router.get('/admin',homeController.admin);
+
+router.use('/user',require('./user'));
+router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
+router.use('/likes', require('./likes'));
+router.use('/adminPanel',require('./adminPanel'));
+router.use('/article',require('./article'));
+
 
 module.exports=router;
