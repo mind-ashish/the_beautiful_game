1d67768f92f75c12bb0f68e04cb624e2762a0342
Aashish Choudhary <aashish01official@gmail.com>
2020-11-06 10:59:18 +0530

    index.js

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
 router.get('/',homeController.home);
+router.get('/login',homeController.login);
+router.get('/signup',homeController.signup);
+router.get('/goodbye',homeController.goodbye);
+router.get('/forgotPassword',homeController.forgotPassword);
+router.get('/changePassword',homeController.changePassword);
 
 module.exports=router;
